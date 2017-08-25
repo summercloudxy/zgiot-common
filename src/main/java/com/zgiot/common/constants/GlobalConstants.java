1d983af592bed50aba9d7dd9de0467c7c41d4a1c
jeickey <jeickey@163.com>
2017-08-25 10:01:35 +0800

    model init.

---
+++ src/main/java/com/zgiot/common/constants/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.zgiot.common.constants;
+
+public class GlobalConstants {
+    public static final String API_VERSION = "/V_1.0";
+    public static final int INVALID_VALUE_INT = -1;
+    public static final String REQUEST_ID_HEADER_KEY = "X-Request-Id";
+}
+
--- src/main/java/com/zgiot/common/pojo/ThingModel.java
+++ src/main/java/com/zgiot/common/pojo/ThingModel.java
@@ -7,31 +7,78 @@ public class ThingModel implements Serializable, Cloneable {
     public static final String CATEGORY_SYSTEM = "SYS";
     public static final String CATEGORY_ERROR = "ERR";
 
-    private String categoryCode;
-    private String code;
-    private String name;
+    public static final String TYPE_DEVICE = "D";
 
-    public String getCode() {
-        return code;
+    private int id;
+    private int parentThingId;
+    private String thingCode;
+    private String thingCategoryCode;
+    private String thingName;
+    private String thingType1Code;
+    private String thingType2Code;
+    private String thingType3Code;
+
+    public int getId() {
+        return id;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public int getParentThingId() {
+        return parentThingId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setParentThingId(int parentThingId) {
+        this.parentThingId = parentThingId;
     }
 
-    public String getCategoryCode() {
-        return categoryCode;
+    public String getThingCode() {
+        return thingCode;
     }
 
-    public void setCategoryCode(String categoryCode) {
-        this.categoryCode = categoryCode;
+    public void setThingCode(String thingCode) {
+        this.thingCode = thingCode;
+    }
+
+    public String getThingCategoryCode() {
+        return thingCategoryCode;
+    }
+
+    public void setThingCategoryCode(String thingCategoryCode) {
+        this.thingCategoryCode = thingCategoryCode;
+    }
+
+    public String getThingName() {
+        return thingName;
+    }
+
+    public void setThingName(String thingName) {
+        this.thingName = thingName;
+    }
+
+    public String getThingType1Code() {
+        return thingType1Code;
+    }
+
+    public void setThingType1Code(String thingType1Code) {
+        this.thingType1Code = thingType1Code;
+    }
+
+    public String getThingType2Code() {
+        return thingType2Code;
+    }
+
+    public void setThingType2Code(String thingType2Code) {
+        this.thingType2Code = thingType2Code;
+    }
+
+    public String getThingType3Code() {
+        return thingType3Code;
+    }
+
+    public void setThingType3Code(String thingType3Code) {
+        this.thingType3Code = thingType3Code;
     }
 }
