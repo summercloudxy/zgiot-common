4ba0f049808d57d172567a6771ff60e3f71b7889
jeickey <jeickey@163.com>
2017-08-29 11:27:36 +0800

    for restcontroller common tools.

---
+++ src/main/java/com/zgiot/common/restcontroller/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.zgiot.common.restcontroller;
+
+import com.zgiot.common.constants.GlobalConstants;
+import com.zgiot.common.exceptions.SysException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(value = SysException.class)
+    public ServerResponse processEdpException(HttpServletRequest req, SysException e) {
+        String reqIdStr = getReqId(req);
+        logger.warn("Request '" + reqIdStr + "' failed: ");
+        return ServerResponse.buildFromException(e);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public ServerResponse processException(HttpServletRequest req, Throwable e) {
+        String reqIdStr = getReqId(req);
+        logger.warn("Request '" + reqIdStr + "' failed: ");
+        return new ServerResponse(e.getMessage(), SysException.EC_UNKOWN,null);
+    }
+
+    private String getReqId(HttpServletRequest req) {
+        String reqIdStr = req.getHeader(GlobalConstants.REQUEST_ID_HEADER_KEY);
+        if (reqIdStr == null) reqIdStr = "";
+        return reqIdStr;
+    }
+
+}
---
+++ src/main/java/com/zgiot/common/restcontroller/ServerResponse.java
@@ -0,0 +1,45 @@
+package com.zgiot.common.restcontroller;
+
+import com.zgiot.common.exceptions.SysException;
+
+public class ServerResponse<T> {
+    private String message;
+    private Integer errorCode;
+    private T data;
+
+    public ServerResponse(String message, Integer errorCode, T data) {
+        this.message = message;
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public static ServerResponse buildFromException(SysException e) {
+        return new ServerResponse(
+                e.getMessage(), e.getErrorCode(), e.getData()
+        );
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
