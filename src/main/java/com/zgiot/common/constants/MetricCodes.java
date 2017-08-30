085f293abb4c47aabd08540875b3b789210f4c2e
jeickey <jeickey@163.com>
2017-08-30 16:46:11 +0800

    update assay metric codes.

--- src/main/java/com/zgiot/common/constants/MetricCodes.java
+++ src/main/java/com/zgiot/common/constants/MetricCodes.java
@@ -7,4 +7,10 @@ public class MetricCodes {
     public static final String CURRENT_2="CR2";
     public static final String LOCAL_BUTTON="LR0"; // 集控就地按钮
 
+    // 化验相关
+    public static final String ASSAY_AAD ="AAD"; // 灰分
+    public static final String ASSAY_MT ="MT"; // 水分
+    public static final String ASSAY_STAD ="STAD"; // 硫分
+    public static final String ASSAY_QNETAR ="QNETAR"; // 发热量
+
 }
