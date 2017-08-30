77ff7559bcf5bbb6022e4c6e75837ad46f7a0d5d
jeickey <jeickey@163.com>
2017-08-30 11:35:20 +0800

    init metric codes.

---
+++ src/main/java/com/zgiot/common/constants/MetricCodes.java
@@ -0,0 +1,10 @@
+package com.zgiot.common.constants;
+
+public class MetricCodes {
+
+    public static final String CURRENT_0="CR0";
+    public static final String CURRENT_1="CR1";
+    public static final String CURRENT_2="CR2";
+    public static final String LOCAL_BUTTON="LR0"; // 集控就地按钮
+
+}
