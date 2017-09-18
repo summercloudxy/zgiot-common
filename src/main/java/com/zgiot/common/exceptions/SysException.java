521290a3370ac228cb131d3982df7527076c64ad
summer cloud <469654418@qq.com>
2017-09-18 09:43:24 +0800

    添加错误码

--- src/main/java/com/zgiot/common/exceptions/SysException.java
+++ src/main/java/com/zgiot/common/exceptions/SysException.java
@@ -3,6 +3,9 @@ package com.zgiot.common.exceptions;
 public class SysException extends RuntimeException{
     public static final int EC_UNKOWN = -1 ; // EC = Error Code
     public static final int EC_SUCCESS = 0 ;
+    public static final int EC_CMD_FAILED = -2;
+    public static final int EC_CMD_PULSE_FIRST_FAILED = -3;
+    public static final int EC_CMD_PULSE_SECOND_FAILED = -4;
 
     private int errorCode;
     private Object data;
