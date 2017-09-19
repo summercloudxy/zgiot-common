6890f8dd9850d4ccc10e45ecf1a7067b168801b5
summer cloud <469654418@qq.com>
2017-09-19 15:33:18 +0800

    固有属性常量定义

---
+++ src/main/java/com/zgiot/common/constants/ThingPropertyConstants.java
@@ -0,0 +1,77 @@
+package com.zgiot.common.constants;
+
+/**
+ * Created by xiayun on 2017/9/18.
+ */
+public class ThingPropertyConstants {
+    private static final String CORRUGATED_ROLLER_DIAMETER = "GEAR_DIA"; // 齿辊直径（mm）
+    private static final String LENGTH = "LENGTH"; // 长度（mm）
+    private static final String FEED_IN_GRANULARITY = "RL_GRA"; // 入料粒度（mm）
+    private static final String DISCHARGE_GRANULARITY = "PL_GRA"; // 排料粒度(mm）
+    private static final String WIDTH_M = "WIDTH_M"; // 宽度（m）
+    private static final String WIDTH_MM = "WIDTH_MM"; // 宽度（mm）
+    private static final String ANGLE = "ANGLE"; // 角度（°）
+    private static final String AMPLITUDE = "AMPLITUDE"; // 振幅（mm）
+    private static final String SIEVE_GAP = "SIEVE_GAP"; // 筛缝（mm）
+    private static final String GROOVE_WIDTH = "GROOVE_WID"; // 槽宽（mm）
+    private static final String SQUEEGEE_WIDTH = "GB_WID"; // 刮板宽度（mm）
+    private static final String SQUEEGEE_HEIGHT = "GB_HEI"; // 刮板高度（mm）
+    private static final String DIAMETER_MM = "DIA_MM "; // 直径（mm）
+    private static final String DIAMETER_M = "DIA_M"; // 直径（m）
+    private static final String SIEVE_BASKET_REVS = "SL_REVS"; // 筛篮转速（r/min）
+    private static final String FILTER_PRESS_AREA = "FP_AREA"; // 压滤面积（平米）
+    private static final String FILTER_PRESS_SIZE = "FP_SIZE"; // 滤板尺寸（mm）
+    private static final String FILTER_PRESS_NUM = "FP_NUM"; // 滤板数量(块）
+    private static final String FILTER_CHAMBER_VOLUME = "FILTER_VOL"; // 滤室容积（立方）
+    private static final String CONCENTRATE_AREA = "CONCEN_AREA"; // 浓缩面积（平米）
+    private static final String DRIVE_FORM = "DRIVE_FORM"; // 驱动形式
+    private static final String BELT_SPEED = "BELT_SPEED"; // 带速（m/s）
+    private static final String PROMOTE_HEIGHT = "PROMO_HEI"; // 提升高度(m）
+    private static final String MATERIEL_GRANULARITY = "MATE_GRA"; // 物料粒度（mm）
+    private static final String FREIGHT_VOLUME = "FREI_VOL"; // 运量（t/h）
+    private static final String SPEED = "SPEED"; // 速度（m/s）
+    private static final String FEED_GRANULARITY = "FEED_GRA"; // 给料粒度(mm）
+    private static final String FLOW = "FLOW"; // 流量（立方/h）
+    private static final String HEAD = "HEAD"; // 扬程（m）
+    private static final String DENSITY = "DENSITY"; // 密度（g/平方厘米）
+    private static final String SEALED_FORM = "MF_FORM"; // 密封形式
+    private static final String EFFICIENCY = "EFFICIENCY"; // 效率（%）
+    private static final String DRIVE_MODE = "DRIVE_MODE"; // 传动方式
+    private static final String BUCKET_CHAIN_PITCH = "CHAIN_PIT"; // 斗链节距（mm）
+    private static final String BUCKET_CHAIN_SPEED = "CHAIN_SPE"; // 斗链速度（m/s）
+    private static final String ROLLER_DIAMETER = "ROLL_DIA"; // 滚筒直径（mm）
+    private static final String ROLLER_LENGTH = "ROLL_LEN"; // 滚筒长度（mm）
+    private static final String ROLLER_SPEED = "ROLL_SPE"; // 滚筒转速（r/min）
+    private static final String ROLLER_NUM = "ROLL_NUM"; // 滚筒数量
+    private static final String APPLICABLE_BANDWIDTH = "APP_BAND"; // 适用带宽（mm）
+    private static final String EXPLOSION_PROOF_GRADE = "FB_GRADE"; // 防爆等级
+    private static final String GAS_SUPPLY = "GAS_SUPPLY"; // 供气量（立方/min）
+    private static final String COOLING_METHOD = "COOL_METHOD"; // 冷却方式
+    private static final String HEIGHT = "HEIGHT"; // 高度（m）
+    private static final String PREPARATION_ABILITY = "ZB_ABILITY"; // 制备能力（L/h）
+    private static final String LIFTING_WEIGHT = "LIFT_WEI"; // 起重量（t）
+    private static final String LIFTING_HEIGHT = "LIFT_HEI"; // 提升高度（m）
+    private static final String LIFTING_SPEED = "LIFT_SPE"; // 提升速度（m/min）
+    private static final String RUN_SPEED = "RUN_SPE"; // 运行速度（m/min）
+    private static final String JOB_LEVEL = "JOB_LEV"; // 工作级别
+    private static final String VOLUME = "VOLUME"; // 容积（立方）
+    private static final String BARREL_HEIGHT = "BARREL_HEI"; // 桶高（m）
+    private static final String WATER_SUPPLY_PRESSURE = "GS_PRES"; // 供水压力(MPa)
+    private static final String WATER_SUPPLY_FLOW = "GS_FLOW"; // 供水流量(m³/h)
+    private static final String SIEVE_SURFACE_RADIAN = "SM_RADIAN"; // 筛面弧度（α°）
+    private static final String SIEVE_SURFACE_WIDTH = "SM_WIDTH;"; // 筛面宽度（mm）
+    private static final String RADIUS_CURVATURE = "RAD_CURV"; // 曲率半径（mm）
+    private static final String SIEVE_MESH_DIAMETER = "SM_DIA"; // 筛网直径（mm）
+    private static final String SIEVE_MESH_LENGTH = "SM_LEN"; // 筛网长度（mm）
+    private static final String REVS = "REVS"; // 转速（r/min）
+    private static final String VOLTAGE = "VOLTAGE"; // 电压(V)
+    private static final String SERIES = "SERIES"; // 级数(级）
+    private static final String MOTOR_NAME = "MOTOR_NAME"; // 电机名称
+    private static final String RATED_POWER = "POWER"; // 额定功率(KW)
+    private static final String RATED_CURRENT = "CURRENT"; // 额定电流（A)
+    private static final String PROTECTION_LEVEL = "FH_LEVEL"; // 防护等级
+    private static final String EXPLOSION_PROOF = "EXPL_PROOF"; // 防爆
+    private static final String INSULATION_LEVEL = "JY_LEVEL"; // 绝缘等级
+    private static final String WAREHOUSE_HEIGHT = "WARE_HEI"; // 仓高（m）
+
+}
