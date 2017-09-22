5ba0d99fe14be79f3625799881beb5134a036511
jeickey <jeickey@163.com>
2017-09-22 14:30:57 +0800

    mock support

--- src/main/java/com/zgiot/common/constants/GlobalConstants.java
+++ src/main/java/com/zgiot/common/constants/GlobalConstants.java
@@ -4,6 +4,7 @@ public class GlobalConstants {
     public static final String API_VERSION = "/V_1.0";
     public static final int INVALID_VALUE_INT = -1;
     public static final String REQUEST_ID_HEADER_KEY = "X-Request-Id";
+    public static final String REQUEST_MOCK_EXP = "X-Mock-Expect";
 
     public static final short STATE_STOPPED = 1;//设备状态-待机
     public static final short STATE_RUNNING = 2;//设备状态-运行
