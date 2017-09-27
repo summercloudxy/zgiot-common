a9b373b896c57dd0b302445605bc8bf4257d7d69
lulansong <lulansong@126.com>
2017-09-27 17:18:44 +0800

    删除常量STATE注释

--- src/main/java/com/zgiot/common/constants/MetricCodes.java
+++ src/main/java/com/zgiot/common/constants/MetricCodes.java
@@ -12,12 +12,7 @@ public class MetricCodes {
     public static final String ASSAY_MT ="MT"; // 水分
     public static final String ASSAY_STAD ="STAD"; // 硫分
     public static final String ASSAY_QNETAR ="QNETAR"; // 发热量
-
-    /**
-     * 以前在FilterPressMetricConstants中，现在移到GlobalConstants中，因为在
-     * FilterPressManager中onDataSourceChange方法会出现metricCode为state状态的非压滤设备，
-     * 在deviceHolder.get（thingCode）时找不到非压滤设备，会出现FilterPress空指针异常
-     */
+    
     public static final String STATE = "STATE"; //状态
 
 }
