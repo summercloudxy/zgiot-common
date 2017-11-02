208d285abe38677c62520efc380b46cafff34f84
lulansong <lulansong@126.com>
2017-11-02 19:59:21 +0800

    压滤日志和呀板数常量

---
+++ src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
@@ -0,0 +1,16 @@
+package com.zgiot.common.constants;
+
+public class FilterPressLogConstants {
+    public static final String FEED_INTELLIGENT = "智能";
+    public static final String FEED_AUTO = "手动";
+    public static final String UNLOAD_INTELLIGENT = "智能";
+    public static final String UNLOAD_AUTO = "手动";
+    public static final int DAY_SHIFT_START_TIME_SCOPE = 8;
+    public static final int DAY_SHIFT_END_TIME_SCOPE = 21;
+    public static final boolean IS_DAY_SHIFT_OK = true;
+    public static final boolean IS_DAY_SHIFT_NO = false;
+    public static final int PERIOD_ONE = 1;
+    public static final int PERIOD_TWO = 2;
+    public static final int RATE_PLATE_COUNT = 24;
+
+}
