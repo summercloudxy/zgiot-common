79c3d22e5b1e548ceff47d73175ec7b3b9796292
lulansong <lulansong@126.com>
2017-11-07 11:50:05 +0800

    压滤压板信息及日志相关常量

--- src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
+++ src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
@@ -6,11 +6,43 @@ public class FilterPressLogConstants {
     public static final String UNLOAD_INTELLIGENT = "智能";
     public static final String UNLOAD_AUTO = "手动";
     public static final int DAY_SHIFT_START_TIME_SCOPE = 8;
-    public static final int DAY_SHIFT_END_TIME_SCOPE = 21;
+    public static final int DAY_SHIFT_END_TIME_SCOPE = 20;
     public static final boolean IS_DAY_SHIFT_OK = true;
     public static final boolean IS_DAY_SHIFT_NO = false;
     public static final int PERIOD_ONE = 1;
     public static final int PERIOD_TWO = 2;
     public static final int RATE_PLATE_COUNT = 24;
+    public static final String CURRENT_DAY = "currentDay";
+    public static final String NEXT_OR_PRIOR_DAY = "nextOrPriorDay";
+    public static final String FILTERPRESS_2492 = "2492";
+    public static final String FILTERPRESS_2493 = "2493";
+    public static final String FILTERPRESS_2494 = "2494";
+    public static final String FILTERPRESS_2495 = "2495";
+    public static final String FILTERPRESS_2496 = "2496";
+    public static final String FILTERPRESS_2496A = "2496A";
+    public static final String FILTERPRESS_2492_RATE_TIME = "00:00";
+    public static final String FILTERPRESS_2493_RATE_TIME = "05:00";
+    public static final String FILTERPRESS_2494_RATE_TIME = "10:00";
+    public static final String FILTERPRESS_2495_RATE_TIME = "15:00";
+    public static final String FILTERPRESS_2496_RATE_TIME = "20:00";
+    public static final String FILTERPRESS_2496A_RATE_TIME = "25:00";
+    public static final String NIGHT_SHIFT_LINE = " 07:59:59";
+    public static final String DAY_SHIFT_START = " 08:";
+    //public static final String NIGHT_SHIFT_START = "20:";
+    public static final int FILTER_PRESS_TOTAL_COUNT = 6;
+    public static final short DAY_ADD_ONE = 1;
+    public static final short DAY_DEC_ONE = -1;
+    public static final int ONE_TEAM_RESET = 1;
+    public static final int TWO_TEAM_RESET = 2;
+    public static final int THREE_TEAM_RESET = 3;
+    public static final String SIG = "SIG";
+    public static final int T1_RCD_POSITION = 6;
+    public static final int T2_RCD_POSITION = 7;
+    public static final int T3_RCD_POSITION = 8;
+    public static final int T1_CHOOSE_POSITION = 6;
+    public static final int T2_CHOOSE_POSITION = 7;
+    public static final int T3_CHOOSE_POSITION = 8;
+    public static final int TEAM_CHOOSE_CLEAN_PERIOD = 1000;
+
 
 }
