83fae93222be4e19c4b804c6b07686137c22d1b2
lulansong <lulansong@126.com>
2017-11-13 15:58:36 +0800

    压滤日志常量

--- src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
+++ src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
@@ -28,10 +28,12 @@ public class FilterPressLogConstants {
     public static final String FILTERPRESS_2496A_RATE_TIME = "25:00";
     public static final String NIGHT_SHIFT_LINE = " 07:59:59";
     public static final String DAY_SHIFT_START = " 08:";
-    //public static final String NIGHT_SHIFT_START = "20:";
+    public static final String NIGHT_SHIFT_START = "20:";
     public static final int FILTER_PRESS_TOTAL_COUNT = 6;
-    public static final short DAY_ADD_ONE = 1;
-    public static final short DAY_DEC_ONE = -1;
+    public static final int DAY_ADD_ONE = 1;
+    public static final int DAY_DEC_ONE = -1;
+    public static final int DAY_DEC_SEVEN = -7;
+    public static final int DAY_DEC_SIX = -6;
     public static final int ONE_TEAM_RESET = 1;
     public static final int TWO_TEAM_RESET = 2;
     public static final int THREE_TEAM_RESET = 3;
@@ -42,7 +44,19 @@ public class FilterPressLogConstants {
     public static final int T1_CHOOSE_POSITION = 6;
     public static final int T2_CHOOSE_POSITION = 7;
     public static final int T3_CHOOSE_POSITION = 8;
-    public static final int TEAM_CHOOSE_CLEAN_PERIOD = 1000;
+    public static final int TEAM_CHOOSE_CLEAN_PERIOD = 1500;
+    public static final String DAY_SHIFT_START_LINE = " 08:00:00";
+    public static final String DAY_SHIFT_END_LINE = " 20:00:00";
+    public static final String NIGHT_SHIFT_MIDDLE_LINE = " 23:59:59";
+    public static final String NIGHT_SHIFT_ZERO_LINE = " 00:00:00";
+    public static final int T1_CHOOSE = 0;
+    public static final int T2_CHOOSE = 1;
+    public static final int T3_CHOOSE = 2;
+    public static final String RATED_START_TIME_DAY_OFFSET = "rated_start_time_day_offset";
+    public static final String RATED_START_TIME_NIGHT_OFFSET = "rated_start_time_night_offset";
+    public static final int CURRENT_DAY_OFFSET = 0;
+    public static final int HIS_PLATE_COUNT_DAY = 7;
+
 
 
 }
