9d887995611a4fb4061eb9856b856f5b5cebed12
lulansong <lulansong@126.com>
2017-11-17 16:49:50 +0800

    压滤日志常量

--- src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
+++ src/main/java/com/zgiot/common/constants/FilterPressLogConstants.java
@@ -9,7 +9,6 @@ public class FilterPressLogConstants {
     public static final int DAY_SHIFT_END_TIME_SCOPE = 20;
     public static final boolean IS_DAY_SHIFT_OK = true;
     public static final boolean IS_DAY_SHIFT_NO = false;
-    public static final int PERIOD_ONE = 1;
     public static final int PERIOD_TWO = 2;
     public static final int RATE_PLATE_COUNT = 24;
     public static final String CURRENT_DAY = "currentDay";
@@ -20,17 +19,9 @@ public class FilterPressLogConstants {
     public static final String FILTERPRESS_2495 = "2495";
     public static final String FILTERPRESS_2496 = "2496";
     public static final String FILTERPRESS_2496A = "2496A";
-    public static final String FILTERPRESS_2492_RATE_TIME = "00:00";
-    public static final String FILTERPRESS_2493_RATE_TIME = "05:00";
-    public static final String FILTERPRESS_2494_RATE_TIME = "10:00";
-    public static final String FILTERPRESS_2495_RATE_TIME = "15:00";
-    public static final String FILTERPRESS_2496_RATE_TIME = "20:00";
-    public static final String FILTERPRESS_2496A_RATE_TIME = "25:00";
+    public static final String FILTERPRESS_2492_RATE_TIME = " 00:00:00";
     public static final String NIGHT_SHIFT_LINE = " 07:59:59";
-    public static final String DAY_SHIFT_START = " 08:";
-    public static final String NIGHT_SHIFT_START = "20:";
     public static final int FILTER_PRESS_TOTAL_COUNT = 6;
-    public static final int DAY_ADD_ONE = 1;
     public static final int DAY_DEC_ONE = -1;
     public static final int DAY_DEC_SEVEN = -7;
     public static final int DAY_DEC_SIX = -6;
@@ -38,9 +29,9 @@ public class FilterPressLogConstants {
     public static final int TWO_TEAM_RESET = 2;
     public static final int THREE_TEAM_RESET = 3;
     public static final String SIG = "SIG";
-    public static final int T1_RCD_POSITION = 6;
-    public static final int T2_RCD_POSITION = 7;
-    public static final int T3_RCD_POSITION = 8;
+    public static final int T1_CLR_POSITION = 6;
+    public static final int T2_CLR_POSITION = 7;
+    public static final int T3_CLR_POSITION = 8;
     public static final int T1_CHOOSE_POSITION = 6;
     public static final int T2_CHOOSE_POSITION = 7;
     public static final int T3_CHOOSE_POSITION = 8;
@@ -56,6 +47,14 @@ public class FilterPressLogConstants {
     public static final String RATED_START_TIME_NIGHT_OFFSET = "rated_start_time_night_offset";
     public static final int CURRENT_DAY_OFFSET = 0;
     public static final int HIS_PLATE_COUNT_DAY = 7;
+    public static final int T1_CHOOSE_VALUE = 32;
+    public static final int T2_CHOOSE_VALUE = 64;
+    public static final int T3_CHOOSE_VALUE = 128;
+    public static final int TEAM1 = 1;
+    public static final int TEAM2 = 2;
+    public static final int TEAM3 = 3;
+
+
 
 
 
