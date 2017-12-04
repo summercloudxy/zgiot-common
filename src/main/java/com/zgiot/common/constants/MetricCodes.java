f05efcab3506b48a03f21c8da569d96e44d86c2f
summer cloud <469654418@qq.com>
2017-12-04 18:16:27 +0800

    修改日志输出

--- src/main/java/com/zgiot/common/constants/MetricCodes.java
+++ src/main/java/com/zgiot/common/constants/MetricCodes.java
@@ -2,20 +2,35 @@ package com.zgiot.common.constants;
 
 public class MetricCodes {
 
-    public static final String CURRENT_0="CR0";
-    public static final String CURRENT_1="CR1";
-    public static final String CURRENT_2="CR2";
-    public static final String LOCAL_BUTTON="LR0"; // 集控就地按钮
+    public static final String CURRENT_0 = "CR0";
+    public static final String CURRENT_1 = "CR1";
+    public static final String CURRENT_2 = "CR2";
+    public static final String LOCAL_BUTTON = "LR0"; // 集控就地按钮
 
     // 化验相关
-    public static final String ASSAY_AAD ="AAD"; // 灰分
-    public static final String ASSAY_MT ="MT"; // 水分
-    public static final String ASSAY_STAD ="STAD"; // 硫分
-    public static final String ASSAY_QNETAR ="QNETAR"; // 发热量
+    public static final String ASSAY_AAD = "AAD"; // 灰分
+    public static final String ASSAY_MT = "MT"; // 水分
+    public static final String ASSAY_STAD = "STAD"; // 硫分
+    public static final String ASSAY_QNETAR = "QNETAR"; // 发热量
 
-    public static final String STATE = "STATE"; //设备状态
-    public static final String RESET = "FRESET"; //故障复位
-    public static final String ALERT_CONFIRM = "ACK"; //报警确认
-    public static final String WARNING = "WARNING"; //报警
+    public static final String STATE = "STATE"; // 设备状态
+    public static final String RESET = "FRESET"; // 故障复位
+    public static final String ALERT_CONFIRM = "ACK"; // 报警确认
+    public static final String WARNING = "WARNING"; // 报警
+
+    public static final String CURRENT_DENSITY = "MD"; // 当前密度
+    public static final String SETTED_DENSITY = "MD_SET"; //设定密度
+    public static final String FLUCTUANT_DENSITY = "444"; //密度波动值
+
+    public static final String CURRENT_LEVEL_M = "LEVEL"; //当前液位（m）
+    public static final String SETTED_HIGH_LEVEL = "1"; //设定高液位
+    public static final String SETTED_LOW_LEVEL = "12"; //设定低液位
+    public static final String PRE_STOP_SETTED_HIGH_LEVEL = "3"; //停车前设定高液位
+    public static final String PROPORTIONAL_VALVE_OPENING = "PRE_READ"; //比例阀开度
+    public static final String PROPORTIONAL_SETTED_HIGH_VALVE_OPENING = "5"; //比例阀高开度设定
+    public static final String PROPORTIONAL_SETTED_LOW_VALVE_OPENING = "4"; //比例阀低开度设定
+    public static final String PRE_STOP_PROPORTIONAL_SETTED_LOW_VALVE_OPENING = "8"; //停车前比例阀低开度设定
+
+
 
 }
