ef4ec085eea0597cc76b89b093805acb34fa62a4
lulansong <lulansong@126.com>
2018-02-09 11:05:10 +0800

    tag常量代码

---
+++ src/main/java/com/zgiot/common/constants/MetricTagConstants.java
@@ -0,0 +1,5 @@
+package com.zgiot.common.constants;
+
+public class MetricTagConstants {
+}
+                                                  
---
+++ src/main/java/com/zgiot/common/constants/ThingTagConstants.java
@@ -0,0 +1,6 @@
+package com.zgiot.common.constants;
+
+public class ThingTagConstants {
+    public static final String EQUIPMENT = "EQUIPMENT";
+}
+                                                  
