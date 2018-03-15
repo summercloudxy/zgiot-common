71a9be19ce293c473d73d794a98601f58be18c50
jiyongsheng <jys138210@163.com>
2018-03-15 15:49:16 +0800

    增加常量 指标等

---
+++ src/main/java/com/zgiot/common/constants/SubscriptionConstants.java
@@ -0,0 +1,58 @@
+package com.zgiot.common.constants;
+
+/**
+ * 订阅常量
+ *
+ * @author jys
+ */
+public class SubscriptionConstants {
+
+
+    public static final String RUN_STATE_START = "运行";
+    public static final String RUN_STATE_STOP = "待机";
+
+    public static final String RUN_STATE_START_1 = "1";
+    public static final String RUN_STATE_STOP_0 = "0";
+
+    public static final String LOAD_STATE_LOAD = "加载";
+    public static final String LOAD_STATE_UNLOAD = "卸载";
+    public static final String LOAD_STATE_LOAD_1 = "1";
+    public static final String LOAD_STATE_LOAD_0 = "0";
+
+    public static final String FAULT = "故障";
+    public static final String FAULT_1 = "1";
+    public static final String FAULT_0 = "0";
+
+    public static final String ASH_CONTENT = "灰分";
+
+    public static final String SULFUR_CONTENT = "硫分";
+
+    public static final String MOISTURE_CONTENT = "水分";
+
+    public static final String ASH_CONTENT_CLEANCOAL_SCOPE = "10.2-10.8";//精煤灰分范围
+    public static final String ASH_CONTENT_MIXED_COAL_SCOPE = "23.5-25";//混煤灰分范围
+
+    public static final String SULFUR_CONTENT_SCOPE = "0.65-0.8";//硫分范围
+
+    public static final String MOISTURE_CONTENT_SCOPE = "0-10";//水分范围
+
+    public static final String FLAG_UP = "UP";//箭头向上
+    public static final String FLAG_DOWN = "DOWN";//箭头向下
+
+    public static final String METRIC_NAME_CT_C = "班"; //班
+    public static final String METRIC_NAME_CT_D = "日"; //日
+    public static final String METRIC_NAME_CT_M = "月"; //月
+    public static final String METRIC_NAME_CT_Y = "年"; //年
+    public static final String METRIC_NAME_CT_T = "总"; //总
+
+
+    public static final String UNIT_T = "t";//单位t
+
+    public static final String UNIT_THOUSAND_T = "万t";//单位万t
+    public static final String RAW_COAL = "原煤";
+    public static final String CLEAN_COAL = "精煤";
+    public static final String MIXED_COAL = "混煤";
+    public static final String SLURRY = "煤泥";
+    public static final String WASTE_ROCK = "矸石";
+
+}
