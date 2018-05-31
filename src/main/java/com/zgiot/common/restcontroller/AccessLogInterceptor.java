35de5033276f5aa5f6f6a578cd5b3fb6abdbafdd
jeickey <zhaojianjun@zhiguaniot.com>
2018-05-31 15:03:56 +0800

    add headers in debug log.

--- src/main/java/com/zgiot/common/restcontroller/AccessLogInterceptor.java
+++ src/main/java/com/zgiot/common/restcontroller/AccessLogInterceptor.java
@@ -9,23 +9,34 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class AccessLogInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(AccessLogInterceptor.class);
-    private static final String START_FLAG="SF_REQ_START_MS";
+    private static final String START_FLAG = "SF_REQ_START_MS";
+
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object o) throws Exception {
 
-        if (logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             Map map = new LinkedHashMap<>();
             map.put("reqId", req.getHeader(GlobalConstants.REQUEST_ID_HEADER_KEY));
-            map.put("from", req.getRemoteHost()+":"+req.getRemotePort());
+            map.put("from", req.getRemoteHost() + ":" + req.getRemotePort());
             map.put("method", req.getMethod());
             map.put("uri", req.getRequestURI());
             map.put("query", req.getQueryString());
             map.put("len", req.getContentLengthLong());
+            // headers
+            Enumeration<String> hNames = req.getHeaderNames();
+            Map headerMap = new LinkedHashMap();
+            while (hNames.hasMoreElements()) {
+                String name = hNames.nextElement();
+                String hValue = req.getHeader(name);
+                headerMap.put(name, hValue);
+            }
+            map.put("headers", headerMap);
 
             req.setAttribute(START_FLAG, System.currentTimeMillis());
             logger.debug("Got Request: `{}`", JSON.toJSONString(map));
@@ -36,7 +47,7 @@ public class AccessLogInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest req, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        if (logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             String reqId = req.getHeader(GlobalConstants.REQUEST_ID_HEADER_KEY);
             long start = (long) req.getAttribute(START_FLAG);
             long ds = System.currentTimeMillis() - start;
