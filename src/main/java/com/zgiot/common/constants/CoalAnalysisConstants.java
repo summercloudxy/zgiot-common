b489532bacdfa534c9c86a843db96fb7dad5286b
xiayun <469654418@qq.com>
2018-06-11 17:18:25 +0800

    修改产品质量统计接口

--- src/main/java/com/zgiot/common/constants/CoalAnalysisConstants.java
+++ src/main/java/com/zgiot/common/constants/CoalAnalysisConstants.java
@@ -7,6 +7,7 @@ public class CoalAnalysisConstants {
     public static final String TCS_TAIL = "TCS尾矿";
     public static final String RAW_COAL = "入洗原煤";
 
+    public static final String FINE_CLEANED_COAL = "末精煤";
     public static final String FIVE_FIVE_ONE_REFINDED="551生产精煤";
     public static final String FIVE_FIVE_TWO_WASHED="552生产洗混煤";
     public static final String FILTER_PRESS_SLIME = "压滤煤泥";
